b854ff6cb93d904d491e211e080b222fa976d244
callistonianembrace <87455417+callistonianembrace@users.noreply.github.com>
2022-06-14 00:46:27 +0530

    E2E: enable tests that wouldn't run on memfs

--- test/e2e-ui/ligo-e2e.spec.js
+++ test/e2e-ui/ligo-e2e.spec.js
@@ -30,18 +30,14 @@ describe('/ (Verdaccio Page)', () => {
   });
 
   test('Search Results: should load No results', async () => {
-    // await page.goto('http://localhost:55558/search/a-pkg-that-doesnt-exist');
-    await page.goto('http://localhost:55558/search/pk1-test');
-    let h1Handle = await page.$('html');
+    await page.goto('http://localhost:55558/search/a-pkg-that-doesnt-exist');
+    let h1Handle = await page.$('h1');
     expect(await h1Handle.evaluate((node) => node.innerText)).toContain('No results');
-    // // TODO
-    // //  error---  error on local search onEnd is not a function
-    // // Not possible to test non-empty list of search results right now, because in-memory backend doesnt implement search
-    // await global.__SERVER__.putPackage(testPackageMetadata.name, testPackageMetadata);
-    // await page.reload({ waitUntil: ['networkidle0', 'domcontentloaded'] });
-    // await page.waitForTimeout(2000);
-    // h1Handle = await page.$('html');
-    // expect(await h1Handle.evaluate((node) => node.innerHTML)).toContain('No results');
+    await page.goto('http://localhost:55558/search/pk1-test');
+    h1Handle = await page.$('h1');
+    expect(await h1Handle.evaluate((node) => node.innerText)).toContain(
+      '1 results found for pk1-test'
+    );
   });
 
   test.skip('Package View: should load package Readme and other details', async () => {
