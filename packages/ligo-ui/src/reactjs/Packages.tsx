83cc11e454675bb95d297a2c6fcecbd9cbc59420
callistonianembrace <87455417+callistonianembrace@users.noreply.github.com>
2022-06-14 22:37:38 +0530

    Style: Restyle Package lists
    On,
    * search
    * landing
    * all packages view

--- packages/ligo-ui/src/reactjs/Packages.tsx
+++ packages/ligo-ui/src/reactjs/Packages.tsx
@@ -2,11 +2,35 @@ import * as React from 'react';
 
 import type { pkg } from '../types';
 import { Header as AppHeader } from './Header';
-import { PackagesList } from './PackagesList';
+import { Link } from './Link';
 
 type props = {
   packages: pkg[];
 };
+
+function PackagesList(props: props) {
+  let { packages } = props;
+  return (
+    <ul className="w-full md:w-2/3 lg:w-1/2 m-auto">
+      {packages.map(({ name, version, description, author }, i) => {
+        return (
+          <li className="rounded-lg border-2 border-slate-800 p-4 m-4 block" key={i}>
+            <Link to={`/package/${name}`}>
+              <div className="card-title">
+                {name} <span className="text-sm italic text-slate-300">{version}</span>{' '}
+              </div>
+              <div>
+                <span className="text-slate-300">By&nbsp;</span>
+                <span className="text-slate-400">{author.name}</span>
+              </div>
+              <p>{description}</p>
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
 
 export function Packages({ packages }: props) {
   if (packages.length === 0) {
